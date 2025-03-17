846ad11720211b40effb4ca2fd9dcf60d9cf31f3
Ignacio Montaldi <87707566+ignacio-montaldi@users.noreply.github.com>
2025-03-17 15:39:11 -0300

    login-terminado

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+import 'package:chat/widgets/logo.dart';
+import 'package:chat/widgets/custom_input.dart';
+import 'package:chat/widgets/labels.dart';
+import 'package:chat/widgets/boton_azul.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xfff2f2f2),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.9,
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Logo(
+                  titulo: 'Registro',
+                ),
+                _Form(),
+                Labels(
+                  ruta: 'login',
+                  titulo: '¿Ya tienes una cuenta?',
+                  subtitulo: 'Ingresa ahora!',
+                ),
+                Text(
+                  'Terminos y condiciones de uso',
+                  style: TextStyle(fontWeight: FontWeight.w200),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  const _Form();
+
+  @override
+  State<_Form> createState() => _FormState();
+}
+
+class _FormState extends State<_Form> {
+  final nameCtrl = TextEditingController();
+  final emailCtrl = TextEditingController();
+  final passwordCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 40),
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: [
+          CustomInput(
+            icon: Icons.person_2_outlined,
+            placeholder: 'Nombre',
+            textController: nameCtrl,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomInput(
+            icon: Icons.mail_outline,
+            placeholder: 'Correo',
+            keyboardType: TextInputType.emailAddress,
+            textController: emailCtrl,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomInput(
+            icon: Icons.lock_outline,
+            placeholder: 'Contraseña',
+            textController: passwordCtrl,
+            isPassword: true,
+          ),
+          const SizedBox(height: 20),
+          BotonAzul(
+            text: "Ingrese",
+            onPressed: () {
+              debugPrint(emailCtrl.text);
+              debugPrint(passwordCtrl.text);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/boton_azul.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class BotonAzul extends StatelessWidget {
+  final String text;
+  final void Function()? onPressed;
+
+  const BotonAzul({super.key, this.onPressed, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      elevation: 2,
+      highlightElevation: 5,
+      color: Colors.blue,
+      shape: const StadiumBorder(),
+      onPressed: onPressed,
+      child: SizedBox(
+        width: double.infinity,
+        height: 55,
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 17,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
